d7affcc712a0ba57b5779aa05549ef094f093a87
erianvc <erianvc.dev@gmail.com>
2020-11-03 21:27:39 -0500

    refactor: separate functions

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-const consumirApi = (criptoMoneda, moneda) => {
-    fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}` )
-      
-        .then((response) => 
-            response.json()
-        )
-        .then((data) => {
-            document.getElementById("res").style.opacity = "1";
-            document.getElementById("res").innerHTML = 
-            "Valor actual = " +data.RAW[criptoMoneda][moneda].PRICE+" " +moneda+"</br>"+
-            "Valor max en las ultimas 24H = " +data.RAW[criptoMoneda][moneda].HIGH24HOUR+" " +moneda+"</br>"+
-            "Valor min en las ultimas 24H = " + data.RAW[criptoMoneda][moneda].LOW24HOUR+" " +moneda+"</br>" ;
-        
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+'use strict'
+window.onload = function() {
+    document.querySelector('#btnCalcular').addEventListener('click', async function() {
+        const criptoMoneda = document.getElementById('criptomoneda').value
+        const moneda = document.getElementById('moneda').value
+
+        const data = await consumirApi(criptoMoneda, moneda)
+        document.getElementById('res').style.opacity = '1'
+        document.getElementById('res').innerHTML = 
+        `Valor actual = ${data.RAW[criptoMoneda][moneda].PRICE} ${moneda}</br>
+        Valor max en las ultimas 24H = ${data.RAW[criptoMoneda][moneda].HIGH24HOUR} ${moneda}</br>
+        Valor min en las ultimas 24H = ${data.RAW[criptoMoneda][moneda].LOW24HOUR} ${moneda}</br>`
+    })
 }
 
-
-document.querySelector("#btnCalcular").addEventListener('click',function(){
-    let criptoMoneda=document.getElementById("criptomoneda").value;
-    let moneda=document.getElementById("moneda").value;
-   consumirApi(criptoMoneda, moneda);
- })
-   
+const consumirApi = (criptoMoneda, moneda) => 
+    fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`)
+        .then(res => res.json())
+        .catch(err => console.log(err))
