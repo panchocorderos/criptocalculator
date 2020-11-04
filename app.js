6f8e3017612e5d1a22f792e892ac38cae609772f
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2020-11-04 14:52:18 -0300

    Corrección de la forma en que era consumida la API

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const consumirApi = (criptoMoneda, moneda) => {
-    fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}` )
-      
-        .then((response) => 
-            response.json()
-        )
-        .then((data) => {
-            document.getElementById("res").style.opacity = "1";
-            document.getElementById("res").innerHTML = 
-            "Valor actual = " +data.RAW[criptoMoneda][moneda].PRICE+" " +moneda+"</br>"+
-            "Valor max en las ultimas 24H = " +data.RAW[criptoMoneda][moneda].HIGH24HOUR+" " +moneda+"</br>"+
-            "Valor min en las ultimas 24H = " + data.RAW[criptoMoneda][moneda].LOW24HOUR+" " +moneda+"</br>" ;
-        
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-}
+  fetch(
+    `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("res").style.opacity = "1";
+      document.getElementById("res").innerHTML = `<h2>El precio es:</h2> 
+      <p>${data.DISPLAY[criptoMoneda][moneda].PRICE}</p>
+      <p>Máximo del día: <span>${data.DISPLAY[criptoMoneda][moneda].HIGH24HOUR}</span></p> 
+      <p>Mínimo del día: <span>${data.DISPLAY[criptoMoneda][moneda].LOW24HOUR}</span></p> 
+      <p>Variación diaria: <span>% ${data.DISPLAY[criptoMoneda][moneda].CHANGEPCTDAY}</span></p>
+      <p>Última actualización: <span>${data.DISPLAY[criptoMoneda][moneda].LASTUPDATE}</span></p>
+      `;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-
-document.querySelector("#btnCalcular").addEventListener('click',function(){
-    let criptoMoneda=document.getElementById("criptomoneda").value;
-    let moneda=document.getElementById("moneda").value;
-   consumirApi(criptoMoneda, moneda);
- })
-   
+document.querySelector("#btnCalcular").addEventListener("click", function () {
+  let criptoMoneda = document.getElementById("criptomoneda").value;
+  let moneda = document.getElementById("moneda").value;
+  consumirApi(criptoMoneda, moneda);
+});
